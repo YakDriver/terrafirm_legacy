6e1776cf1cd30bfdba8f2a1b37a947e8b989dce9
YakDriver <31492422+YakDriver@users.noreply.github.com>
2017-12-22 09:38:05 -0500

    Update terrafirm.tf

--- terrafirm.tf
+++ terrafirm.tf
@@ -1,5 +1,13 @@
 resource "aws_instance" "centos6" {
   ami = "ami-bfb356d2"
   instance_type = "t1.micro"
+  
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.centos6.public_ip} > ip_address.txt"
+  }
+  
+  provisioner "local-exec" {
+    command = "cat ip_address.txt"
+  }
 }
 
