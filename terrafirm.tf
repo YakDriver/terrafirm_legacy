479b1907abd3e84c06ae675b85fbe5a3bef4aa85
YakDriver <31492422+YakDriver@users.noreply.github.com>
2017-12-22 18:49:10 -0500

    Update terrafirm.tf

--- terrafirm.tf
+++ terrafirm.tf
@@ -2,6 +2,11 @@ resource "aws_instance" "centos6" {
   ami = "ami-2b8ce651"
   instance_type = "t1.micro"
   
+  timeouts {
+    create = "20m"
+    delete = "10m"
+  }
+  
   connection {
     type     = "ssh"
     user     = "maintuser"
