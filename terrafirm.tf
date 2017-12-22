1537774054be66eb40d5a717836330e4ec82ef27
YakDriver <31492422+YakDriver@users.noreply.github.com>
2017-12-22 18:23:16 -0500

    Update terrafirm.tf

--- terrafirm.tf
+++ terrafirm.tf
@@ -2,6 +2,12 @@ resource "aws_instance" "centos6" {
   ami = "ami-2b8ce651"
   instance_type = "t1.micro"
   
+  connection {
+    type     = "ssh"
+    user     = "maintuser"
+    password = "${var.root_password}"
+  }
+  
   provisioner "remote-exec" {
     script = "watchmaker.sh"
   }
