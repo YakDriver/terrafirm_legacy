8499addf545a8108aaa727c402c5f6884e97bdf0
YakDriver <31492422+YakDriver@users.noreply.github.com>
2017-12-26 12:16:12 -0500

    Update terrafirm.tf

--- terrafirm.tf
+++ terrafirm.tf
@@ -1,9 +1,4 @@
-provider "aws" {
-  region = "us-east-1"
-  assume_role {
-    role_arn     = "arn:aws:iam::701759196663:role/codebuild-terrafirm-service-role"
-  }
-}
+provider "aws" {}
 
 resource "aws_instance" "centos6" {
   #ami = "ami-2b8ce651" #spel partitioned
