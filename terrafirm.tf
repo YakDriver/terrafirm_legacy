0a3564cbb52c125109f5497a8356fda534a272e4
YakDriver <31492422+YakDriver@users.noreply.github.com>
2017-12-28 17:07:01 -0500

    Update terrafirm.tf

--- terrafirm.tf
+++ terrafirm.tf
@@ -5,18 +5,6 @@ resource "aws_key_pair" "auth" {
   public_key = "${var.public_key}"
 }
 
-# Create a VPC for instance
-#resource "aws_vpc" "default" {
-#  cidr_block = "10.0.0.0/16"
-#}
-
-# Subnet for instance
-#resource "aws_subnet" "default" {
-#  vpc_id                  = "${aws_vpc.default.id}"
-#  cidr_block              = "10.0.1.0/24"
-#  map_public_ip_on_launch = true
-#}
-
 # Security group to access the instances over SSH
 resource "aws_security_group" "terrafirm" {
   name        = "terrafirm_sg"
