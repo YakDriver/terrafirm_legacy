cb14745f0f4123b4b0a12f1d7bcee9dc0aea8cc9
YakDriver <31492422+YakDriver@users.noreply.github.com>
2017-12-28 18:24:31 -0500

    Update watchmaker.sh

--- watchmaker.sh
+++ watchmaker.sh
@@ -9,14 +9,14 @@ wait_file() {
   local file="$1"; shift
   local wait_seconds="${1:-10}"; shift # 10 seconds as default timeout
 
-  until test $((wait_seconds--)) -eq 0 -o -f "$file" ; do sleep 1; done
+  until test $((wait_seconds--)) -eq 0 -o -f "$file" ; do sleep 5; done
 
   ((++wait_seconds))
 }
 
 exec_file=/usr/bin/watchmaker
 
-wait_file "$exec_file" 300 || {
+wait_file "$exec_file" 100 || {
   echo "Executable on remote instance never became available for $? seconds: '$exec_file'"
   exit 1
 }
